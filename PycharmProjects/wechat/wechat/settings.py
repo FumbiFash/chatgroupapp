dfc6bcca302d78aa09115d550fca51e45c5b8e56
Fumbi <fumbifash@ymail.com>
2023-02-02 15:02:28 +0000

    second commit

--- PycharmProjects/wechat/wechat/settings.py
+++ PycharmProjects/wechat/wechat/settings.py
@@ -148,6 +148,6 @@ MEDIA_ROOT = BASE_DIR/"static/images"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
